cd0077de564c35f275d18d42e73f222281a3ed86
faridlan <faridlannulhakim@gmail.com>
2022-09-10 23:54:09 +0700

    edit index.go

--- api/index.go
+++ api/index.go
@@ -1,10 +1,28 @@
 package handler
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
+type User struct {
+	Id   int    `json:"id,omitempty"`
+	Name string `json:"name,omitempty"`
+	Age  int    `json:"age,omitempty"`
+}
+
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+	// fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+	w.Header().Add("content-type", "application/json")
+	faridlan := User{
+		Id:   1,
+		Name: "Faridlan",
+		Age:  21,
+	}
+
+	encode := json.NewEncoder(w)
+	err := encode.Encode(&faridlan)
+	if err != nil {
+		panic(err)
+	}
 }
---
+++ app/database/db.sql
@@ -0,0 +1,3 @@
+-- Active: 1662825965016@@128.199.67.28@3306@todo_list
+
+show tables;
