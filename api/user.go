a2cede37aac6a602c6761714da9984e0f7951b54
faridlan <faridlannulhakim@gmail.com>
2022-10-01 23:35:41 +0700

    fixing mux

--- api/user.go
+++ api/user.go
@@ -11,6 +11,7 @@ func HelloWorld(writer http.ResponseWriter, request *http.Request) {
 
 	r := mux.NewRouter()
 
+	r.HandleFunc("/api/user", helper.Hello).Methods("GET")
 	r.HandleFunc("/", helper.Hello).Methods("GET")
 
 	http.ListenAndServe(":80", r)
